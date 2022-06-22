3c21b58b19b272f1bd293fc26444e6fa0c549bbe
Jacob <jacobmjones17@gmail.com>
2022-06-22 04:09:22 -0400

    installed JSON server and Created QuestionList, Question, Form component. Still working on getting functionality working properly

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import Question from "./components/Question";
+import QuestionList from "./components/QuestionList";
 
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
+      <QuestionList />
     </div>
   );
 }
