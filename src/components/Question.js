0928b73d79490760548ed4dc549b34128640ff9f
Jacob <jacobmjones17@gmail.com>
2022-06-22 03:47:25 -0400

    Created the JSON server. Will add more to it later

---
+++ src/components/Question.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+
+function Question({ question, onAnswered }) {
+    const [timeRemaining, setTimeRemaining] = useState(10);
+
+    useEffect(() => {
+        if (timeRemaining === 0) {
+            setTimeRemaining(10)
+            onAnswered(false)
+            return;
+        }
+
+        const timerId = setTimeout(() => {
+            setTimeRemaining((seconds) => seconds - 1);
+        }, 1000);
+
+        return () => {
+            clearTimeout(timerId);
+            console.log(timeRemaining)
+        };
+    }, [timeRemaining, onAnswered])
+
+    function handleAnswer(isCorrect) {
+        setTimeRemaining(10);
+        onAnswered(isCorrect);
+    }
+
+    const { id, prompt, answers, correctIndex } = question;
+
+    return (
+        <>
+            <h1>Question {id}</h1>
+            <h3>{prompt}</h3>
+            {answers.map((answer, index) => {
+                const isCorrect = index === correctIndex;
+                return (
+                    <button key={answer} onClick={() => handleAnswer(isCorrect)}>
+                        {answer}
+                    </button>
+                );
+            })}
+            <h5>{timeRemaining} seconds remaining</h5>
+        </>
+    );
+}
+
+export default Question;
