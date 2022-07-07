1f45bbac18653aba14c89d2a1fb46cfd1cbeceb7
Jacob <jacobmjones17@gmail.com>
2022-07-07 17:13:08 -0400

    I am completely done with the project

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Home() {
+
     return (
         <div className="description">
-        <p>Your one stop shop for learning React!</p>
+        <p>Your one-stop shop for learning React!</p>
         </div>
     )
 }
